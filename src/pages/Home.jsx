82f9a5dd071cdecf1e367776a402b267b80c5bc2
Hassaan-Ahmedd <hassutechap@gmail.com>
2023-09-04 18:32:33 +0500

    Setup Completed + working on Navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home=()=>{
+
+return(
+
+<>
+
+
+<Navbar/>
+
+<h1 style={{textAlign:"center"}}>Welocme to Home Page </h1>
+
+</>
+
+)
+
+}
+
+export default Home
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from  '../images/logo.png'
+
+const Navbar = () => {
+  return (
+    <>
+    
+    
+    
+    <div className='h-[5.3vw] w-[100%]  shadow-gray-400 shadow-md'>
+
+
+
+{/* For Title & Logo */}
+<div className='w-[20%] h-[100%] bg-slate-300 flex '>
+
+<img src={logo} alt="" />
+
+<h1>Ideal Store</h1>
+
+</div>
+
+
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default Navbar
